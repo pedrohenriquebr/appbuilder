f7d7b70cc149d5316ff92ce0790b43c3cc340cbb
pedrohenriquebr <pedrohenriquebraga735@gmail.com>
2022-09-03 02:12:26 -0300

    remove java

--- src/main/java/appbuilder/api/annotations/Annotations.java
+++
@@ -1,9 +0,0 @@
-package appbuilder.api.annotations;
-
-public  final class Annotations {
-    public static final int OVERRIDE = 255;
-
-    public static final boolean HasAnnotation(int valor , int anotação){
-        return (valor & anotação) == anotação;
-    }
-}
